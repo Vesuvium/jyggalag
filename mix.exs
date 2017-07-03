b178942ea8144a9e301bdae66b7e7f801d3d7e20
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-07-04 00:06:11 +0200

    chore(jyggalag): prepare for publication

--- mix.exs
+++ mix.exs
@@ -2,22 +2,46 @@ defmodule Jyggalag.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :jyggalag,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :jyggalag,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Jyggalag",
+      source_url: "https://github.com/Vesuvium/jyggalag"
+    ]
   end
 
   defp deps do
-    [{:git_cli, "~> 0.2"},
-     {:poison, "~> 3.1"},
-     {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    [
+      {:git_cli, "~> 0.2"},
+      {:poison, "~> 3.1"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
     ]
   end
 
   def application do
     [applications: [:logger]]
+  end
+
+  defp description do
+    """
+    A command line project tracker meant to bring order and peace.
+    """
+  end
+
+  defp package do
+    [
+      name: :jyggalag,
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Jacopo Cascioli"],
+      licenses: ["GPL 3"],
+      links: %{"GitHub" => "https://github.com/Vesuvium/jyggalag"}
+    ]
   end
 end
